a73f98ec0b4d2aaa463e9d1e7c702b1b7dbfd01e
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2016-06-05 20:39:54 +0200

    JS code cleaning and corresponds more to the contract

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,71 +6,56 @@ var numMembers;
 function setStatus(message) {
   var status = document.getElementById("status");
   status.innerHTML = message;
-};
+}
 
 function refreshNumMembers() {
   var reputation = Reputation.deployed();
 
-  reputation.getNumMembers.call(account, {from: account}).then(function(value) {
-    var members_element = document.getElementById("numMembers");
-    members_element.innerHTML = value.valueOf();
-    numMembers = value.valueOf();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error getting number of members; see log.");
-  });
-};
-
-function createMember() {
-    var reputation = Reputation.deployed();
-    reputation._initialMember(account,{from: account}).then(function(){
-	setStatus("Creating initial Member...");
-    }	
-    ).catch(function(e) {
-	console.log(e);
-	setStatus("Error creating member; see log.");
-    });
-}
-
-function addNode(){
-    var reputation = Reputation.deployed();
-    var newGuy = document.getElementById("newGuy").value;
-    setStatus(newGuy);
-    
-    setStatus("Adding new guy... (please wait)");
-    
-    reputation.createMember(newGuy, {from: account}).then(function(b) {
-	if(b){
-	setStatus("Added new guy!");
-	}
-	else{
-	setStatus("Failed adding new guy, maybe he already exists?");
-	}
-	refreshNumMembers();
-	return(true);
+  reputation.getNumMembers.call(account, {from: account})
+    .then(function(value) {
+      var members_element = document.getElementById("numMembers");
+      members_element.innerHTML = value.valueOf();
+      numMembers = value.valueOf();
     }).catch(function(e) {
-	console.log(e);
-	setStatus("Error adding new guy; see log.");
+      console.log(e);
+      setStatus("Error getting number of members; see log.");
     });
-    
 }
 
-// function sendCoin() {
-//   var meta = MetaCoin.deployed();
-
-//   var amount = parseInt(document.getElementById("amount").value);
-//   var receiver = document.getElementById("receiver").value;
-
-//   setStatus("Initiating transaction... (please wait)");
-
-//   meta.sendCoin(receiver, amount, {from: account}).then(function() {
-//     setStatus("Transaction complete!");
-//     refreshBalance();
-//   }).catch(function(e) {
+// function createMember() {
+//   var reputation = Reputation.deployed();
+//   reputation._initialMember(account,{from: account}).then(function(){
+//     setStatus("Creating initial Member...");
+//   }	
+//   ).catch(function(e) {
 //     console.log(e);
-//     setStatus("Error sending coin; see log.");
+//     setStatus("Error creating member; see log.");
 //   });
-// };
+// }
+
+function addMember(){
+  var reputation = Reputation.deployed();
+
+  var newGuy = document.getElementById("newGuy").value;
+  setStatus(newGuy);
+
+  setStatus("Adding new guy... (please wait)");
+
+  // NB: this is going to be a ``sendTransaction``, so it's not possible to use
+  // the return value here
+  reputation.createMember(newGuy, {from: account})
+    .then(function() {
+      setStatus("Added new guy (if he was new)!");
+      refreshNumMembers();
+      return(true);
+    })
+    .catch(function(e) {
+      console.log(e);
+      setStatus("Error adding new guy; see log.");
+    });
+}
+
+
 
 window.onload = function() {
   web3.eth.getAccounts(function(err, accs) {
@@ -87,10 +72,10 @@ window.onload = function() {
     accounts = accs;
     account = accounts[0];
 
-    if(numMembers == 0){
-	createMember();
-    }
+    // if(numMembers == 0){
+    //   createMember();
+    // }
     refreshNumMembers();
 
   });
-}
+};
